25a498bd4601839d21a30d50a35d6514268f2237
Caio Espindola <caioespindola@hotmail.com>
2024-01-17 11:51:02 -0300

    api integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='bg-red-100 min-h-screen flex flex-col items-center'>
+          <nav className='w-full bg-blue-300 h-16 flex items-center justify-center'>
+            <section className='container flex items-center gap-3'>
+              <Image 
+                width={48}
+                height={48}
+                src="/world2.jpg" 
+                alt="logo">                
+              </Image>
+              <h1 className='font-bold text-2xl'>Country List</h1>              
+            </section>
+          </nav>
+          {children}
+        </main>       
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-import Image from 'next/image'
+type Country = {
+  name: {
+    common: string
+  }
+}
 
-export default function Home() {
+async function getCountry(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json()
+}
+
+export default async function Home() {
+  const countries = await getCountry() 
+
   return (
-    <h1>Home: Hello World!</h1>
+    <section className='container flex w-full'>         
+      {countries.map((country) => (
+      <h1 key={country.name.common}>{country.name.common}</h1>
+      ))}
+    </section>    
   )
 }
